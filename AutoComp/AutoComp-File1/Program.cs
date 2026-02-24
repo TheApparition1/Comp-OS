bdfb8f5418eeca0212d864ee21a53a5614b57a8e
NewGameDev <jordansamuel893@gmail.com>
2026-02-24 16:47:54 +1100

    Add files via upload
    Work in progress. Rider files for AutoComp(automatically runs wine when exe or msi files are attempted to be used.)

---
+++ AutoComp/AutoComp-File1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using System;
+using System.IO;
+#!/usr/bin/env -S dotnet run
+
+string executableType;
+string file;
+string command;
+string consoleName = "kitty.deb";
+void FileOpened()
+{
+    executableType = "exe";
+    if(Convert.ToBoolean(executableType = "exe"))
+    {
+        RunWithWine();
+    }
+    else if (Convert.ToBoolean(executableType = "msi"))
+    {
+        RunWithWine();
+    }
+    else
+    {}
+}
+
+void RunWithWine()
+{ 
+    command = "wine " + file;
+    Console.Write(command);
+    Console.Write("exit");
+}
