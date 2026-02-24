43f08babfe7c7b203250a291b2bbb13d55c3a683
Samuel Dingle <TheApparition1@outlook.com>
2026-02-24 19:54:41 +1100

    Refactor file handling and command execution logic
    Refactor Program.cs to improve command execution with wine.

--- AutoComp/AutoComp-File1/Program.cs
+++ AutoComp/AutoComp-File1/Program.cs
@@ -1,30 +1,45 @@
-﻿using System.Diagnostics;
 using System;
-using System.IO;
+using System.Diagnostics;
+
 #!/usr/bin/env -S dotnet;
 
 string executableType;
-string file;
+string file = "kitty.deb";
 string command;
 string consoleName = "kitty.deb";
+
+FileOpened();
+
 void FileOpened()
 {
-    executableType = "exe";
-    if(Convert.ToBoolean(executableType = "exe"))
+    executableType = "exe"; 
+    if (executableType == "exe")
     {
         RunWithWine();
     }
-    else if (Convert.ToBoolean(executableType = "msi"))
+    else if (executableType == "msi")
     {
         RunWithWine();
     }
     else
-    {}
+    {
+    }
 }
 
-void RunWithWine()
-{ 
+void RunWithWine();
+{
     command = "wine " + file;
-    Console.Write(command);
-    Console.Write("exit");
+    Console.WriteLine("Command to run: " + command);
+    
+    var process = new Process();
+    process.StartInfo.FileName = "wine";
+    process.StartInfo.Arguments = file;
+    process.StartInfo.UseShellExecute = false;
+    process.StartInfo.RedirectStandardOutput = true;
+    process.Start();
+
+    string output = process.StandardOutput.ReadToEnd();
+    process.WaitForExit();
+
+    Console.WriteLine(output);
 }
