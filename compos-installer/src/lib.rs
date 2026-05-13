c43e38bf7d0b076cd5139fd0aacff6b9d33d986c
Samuel Dingle <TheApparition1@outlook.com>
2026-05-13 10:34:54 +1000

    Added more edits and set up netlify conf files

---
+++ compos-installer/src/lib.rs
@@ -0,0 +1,21 @@
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri::plugin::prelude::Builder::default().build())
+        .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            #[cfg(debug_assertions)]
+            {
+                let window = app.get_webview_window("main").unwrap();
+                window.open_devtools();
+            }
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
